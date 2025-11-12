08e4fd2f71326dc8449f5d375e77df5f576d6d2e
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-11-12 12:53:46 +0200

    setup app layout

---
+++ app/(screens)/(home)/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'expo-router';
+import { Pressable, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Link href={{ pathname: '/(screens)/(game)/[id]', params: { id: 123 } }} asChild>
+        <Pressable>
+          <Text style={{ color: 'white' }}>GAME</Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
+}

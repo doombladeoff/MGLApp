6e8d2bb15213da0b20e0f71a75b7a258698250df
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-11-15 23:45:07 +0200

    add user placeholder image

--- app/(screens)/(home)/(tabs)/_layout.tsx
+++ app/(screens)/(home)/(tabs)/_layout.tsx
@@ -5,13 +5,14 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { useUser } from '@clerk/clerk-expo';
 import { BlurView } from 'expo-blur';
 import { Image } from 'expo-image';
 import { Pressable, StyleSheet, View } from 'react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const { user } = useUser();
 
   return (
     <Tabs
@@ -40,17 +41,23 @@ export default function TabLayout() {
               >
                 <IconSymbol name='magnifyingglass' size={28} color={'white'} />
               </Pressable>
-              <Pressable
-                hitSlop={10}
-                onPress={() => router.push({ pathname: '/user/[id]', params: { id: 1 } })}
-                style={({ pressed }) => ({
-                  top: -2,
-                  right: 15,
-                  opacity: pressed ? 0.8 : 1
-                })}
-              >
-                <Ionicons name="person" size={28} color="white" />
-              </Pressable>
+              {user && (
+                <Pressable
+                  hitSlop={10}
+                  onPress={() => router.push({ pathname: '/user/[id]', params: { id: 1 } })}
+                  style={({ pressed }) => ({
+                    top: -2,
+                    right: 15,
+                    opacity: pressed ? 0.8 : 1
+                  })}
+                >
+                  <Image
+                    source={user?.hasImage ? { uri: user?.imageUrl } : require('@/assets/images/user-placeholder.png')}
+                    style={{ width: 30, height: 30, borderRadius: 100, backgroundColor: 'gray' }}
+                    transition={400}
+                  />
+                </Pressable>
+              )}
             </View>
           ),
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,

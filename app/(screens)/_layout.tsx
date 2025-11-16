7ac6af4f9eaba93240fe9008f2235ffcc9b22d96
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2025-11-16 23:46:51 +0200

    update layout

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -1,22 +1,45 @@
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { useAuth } from '@clerk/clerk-expo';
 import { Stack } from "expo-router";
+import React from "react";
+import { PlatformColor, TouchableOpacity } from "react-native";
 
 export default function ScreensLayout() {
+  const { signOut } = useAuth();
+
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerTitle: '',
+        headerBackTitle: 'Назад',
+        contentStyle: { backgroundColor: '#0f0f0f' },
+        headerStyle: { backgroundColor: '#0f0f0f' }
+      }}
+    >
       <Stack.Screen name="(home)" options={{ headerShown: false }} />
       <Stack.Screen
         name="(game)/[id]"
         options={{
-          headerTitle: '',
           headerTransparent: true,
-          headerBackTitle: 'Назад'
+          headerStyle: { backgroundColor: undefined },
+          contentStyle: { backgroundColor: 'black' },
         }}
       />
       <Stack.Screen
         name="user/[id]"
         options={{
-          headerTitle: '',
-          headerBackTitle: 'Назад'
+          headerRight: () => (
+            <TouchableOpacity hitSlop={10} onPress={() => signOut()}>
+              <IconSymbol name='rectangle.portrait.and.arrow.right.fill' size={30} color={PlatformColor('red')} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+
+      <Stack.Screen
+        name="user/profile-edit"
+        options={{
+          headerBackTitle: 'Профиль'
         }}
       />
     </Stack>
